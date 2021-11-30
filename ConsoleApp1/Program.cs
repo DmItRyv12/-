65bae2addbe01b6e18a6ebf5f244177c6a92c033
Дмитрий Минаков <m1nak0vd@yandex.ru>
2021-11-30 14:54:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0, e = 1;
+            Console.Write("N=");
+            int n = int.Parse(Console.ReadLine());
+            while (e <= n * 2)
+            {
+                s += e;
+                Console.WriteLine(s.ToString());
+                e += 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Повторное решение задания/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Повторное_решение_задания
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, k, s1; string s;
+            Console.Write("Введите целое число(N>0) N=");
+            s = Console.ReadLine();
+            N = Convert.ToInt32(s);
+            Console.WriteLine("Квадраты чисел от 1 до N:");
+            s1 = 0;
+            for (k = 1; k <= (2 * N - 1); k = k + 2)
+            {
+                s1 = s1 + k;
+                Console.Write("{0}, ", s1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
